69ac7ab0150e801afbde71b762c18feb5c9d5361
Congeladorportatil <41164425+Congeladorportatil@users.noreply.github.com>
2025-03-17 06:59:04 +0100

    Update AnalizadorBasico.java

--- src/es/upm/dit/adsw/ajedrez25/analizadores/AnalizadorBasico.java
+++ src/es/upm/dit/adsw/ajedrez25/analizadores/AnalizadorBasico.java
@@ -16,14 +16,25 @@ public class AnalizadorBasico {
 	private static final Logger LOGGER = Logger.getLogger(LectorPartidas.class.getName());
 	List<Partida> partidas;
 	List<Tablero> tableros = new ArrayList<Tablero>();
+	Map<String, List<Partida>> jugadoresPartida = new HashMap<String, List<Partida>>();
+	List<String> jugadores;
 	
 	public AnalizadorBasico (List<Partida> partidas) {
+		this.jugadoresPartida = new HashMap<>();
 		this.partidas = partidas;
 		for (Partida p: partidas) {
 			for (Tablero t : p.getTurnos()) {
 					tableros.add(t);
 			}
+			if (jugadoresPartida.containsKey(p.getJugadorBlancas())) {
+	            jugadoresPartida.get(p.getJugadorBlancas()).add(p);
+	        } else {
+	            List<Partida> partidasJugador = new ArrayList<>();
+	            partidasJugador.add(p);
+	            jugadoresPartida.put(p.getJugadorBlancas(), partidasJugador);
+	        }
 		}
+		jugadores = new ArrayList<>(jugadoresPartida.keySet());
 		ordenarTableros();
 	}
 	
@@ -63,26 +74,71 @@ public class AnalizadorBasico {
 		 list.add(right.remove(0));
 		}
 	
+	public int buscarTablerosPorPuntuacion(int puntuacion) {
+        int inicio = 0;
+        int fin = tableros.size() - 1;
+		
+        while (inicio <= fin) {
+        	int medio = (inicio + fin) /2;
+            int puntuacionMedio = tableros.get(medio).getPuntuacionGeneral();
+            
+            if (puntuacionMedio == puntuacion) {
+            	while (medio > 0 && tableros.get(medio -1).getPuntuacionGeneral() == puntuacion) {
+            		medio--;
+            	}
+            	return medio;
+            }
+            else if (puntuacionMedio < puntuacion)
+            	inicio = medio +1;
+            else 
+            	fin = medio-1;
+        }
+        return inicio;
+    }
+
+	
+	
 	public Tablero getMayorTablero() {
 		return tableros.get(tableros.size()-1);
 	}
 	
 	public int getPuntuacionMediana( ) {
-		if (tableros.size()> 0)
-			return tableros.get((int)(tableros.size()/2)).getPuntuacionGeneral();
-		else
-			return 0;
+		int mediana = tableros.size()/2;
+		if (mediana % 2 == 0) {
+			return tableros.get(mediana).getPuntuacionGeneral();
+		}
+		else {
+			int pos1 = tableros.get(mediana).getPuntuacionGeneral();
+			int pos2 = tableros.get(mediana + 1).getPuntuacionGeneral();
+			return (pos1 + pos2) / 2 ;
+		}
+	}
+	
+	public int getNTurnosPartidaMasCorta() {
+		int min = Integer.MAX_VALUE;
+		for (Partida p : partidas) {
+			int nturnos = p.getTurnos().size();
+			if (nturnos < min) {
+				min = nturnos;
+			}
+		}
+		return min;
+	}
+	
+	public int getPartidasGanadasPor(String jugador) {
+		return -1;
 	}
 	
 	public static void main(String[] args) throws Exception {
-	    LectorPartidas lector = new LectorPartidas("data/partidas.txt");
+	    LectorPartidas lector = new LectorPartidas("data/puntuacion.txt");
 	    long t = System.currentTimeMillis();
 	    AnalizadorBasico basico = new AnalizadorBasico(lector.getPartidas());
 	    LOGGER.info("Tiempo de análisis: " + (System.currentTimeMillis() - t) + " ms");
-	    LOGGER.info("El mayor tablero es: "  + 	basico.getMayorTablero() + "con una puntuación de " + basico.getMayorTablero().getPuntuacionGeneral());
-	    //LOGGER.info("Número de turnos de la partida más corta: " + basico.getNumTurnosPartidaMasCorta());
-	    LOGGER.info("Puntuación mediana: " + basico.getPuntuacionMediana());
-	    //LOGGER.info("Partidas ganadas por mahdii: " + basico.getPartidasGanadasPor("mahdii"));
+	    LOGGER.info("hola "+ basico.buscarTablerosPorPuntuacion(+5));
+	    //LOGGER.info("El mayor tablero es: "  + 	basico.getMayorTablero() + "con una puntuación de " + basico.getMayorTablero().getPuntuacionGeneral());
+	    LOGGER.info("Número de turnos de la partida más corta: " + basico.getNTurnosPartidaMasCorta());
+	    //LOGGER.info("Puntuación mediana: " + basico.getPuntuacionMediana());
+	    //LOGGER.info("Partidas ganadas por mahdii: " + basico.getPartidasGanadasPor("TrialB"));
 	    //LOGGER.info("Mejor jugador: " + basico.getMejorJugador() + " con " + basico.getPartidasGanadasPor(basico.getMejorJugador()) + " victorias");
 	}
 }
